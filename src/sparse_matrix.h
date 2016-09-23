a08a3bbd50a8a7cd6b9c0f2d567d76a417f0fa6b
fanwangwang <646043316@qq.com>
2016-09-23 16:00:40 +0800

    update

--- src/sparse_matrix.h
+++ src/sparse_matrix.h
@@ -5,39 +5,39 @@ class Sparse_matrix
 public:
 	Sparse_matrix()
 	{
-	int size = 6;
-	AA = new int[size];
-	AA[0] = 1;
-	AA[1] = 2;
-	AA[2] = 3;
-	AA[3] = 4;
-	AA[4] = 5;
-	AA[5] = 6;
-	JA = new int[size];
-	JA[0] = 0;
-	JA[1] = 1;
-	JA[2] = 1;
- 	JA[3] = 0;
-	JA[4] = 2;
-	JA[5] = 3;
-	IA = new int[size];
-	IA[0] = 0;
-	IA[1] = 1;
-	IA[2] = 2;
-	IA[3] = 3;
-	IA[4] = 6;
+        int size = 6;
+        AA = new int[size];
+        AA[0] = 1;
+        AA[1] = 2;
+        AA[2] = 3;
+        AA[3] = 4;
+        AA[4] = 5;
+        AA[5] = 6;
+        JA = new int[size];
+        JA[0] = 0;
+        JA[1] = 1;
+        JA[2] = 1;
+        JA[3] = 0;
+        JA[4] = 2;
+        JA[5] = 3;
+        IA = new int[size];
+        IA[0] = 0;
+        IA[1] = 1;
+        IA[2] = 2;
+        IA[3] = 3;
+        IA[4] = 6;
 	}
 	
 	~Sparse_matrix()
 	{
-	delete AA[];
-	delete JA[];
-	delete IA[];
+        delete AA[];
+        delete JA[];
+        delete IA[];
 	}
 	
 	void print()
 	{
-	cout << Sparse_matrix[i][j] << endl;
+        cout << Sparse_matrix[i][j] << endl;
 	}
 private:
 	int *AA;
